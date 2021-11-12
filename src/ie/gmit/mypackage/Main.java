735e61bcf27afb69476448942384e85ecfcc7de9
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-11-12 12:22:27 +0000

    Cleaned up code

--- src/ie/gmit/mypackage/Main.java
+++ src/ie/gmit/mypackage/Main.java
@@ -2,36 +2,19 @@
  *  Compilation:  javac Main.java
  *  Execution:    java Main
  *
- *  Prints "Hello, World" to console when "Say Hello World" button is pressed.
+ *  Prints "Hello World!" to console.
  *
  ******************************************************************************/
 package ie.gmit.mypackage;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(e -> {
-            System.out.println("Hello World!");
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
     
+    // This is the main method.
     public static void main(String[] args) {
-        launch(args);
-    }
-
-}
+        
+        // This statement print "Hello World" to the console.
+        System.out.println("Hello World!");
+    
+    } // End main method
+    
+} // End Main class
