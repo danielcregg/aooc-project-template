bc847c6bd73b733670c1bb80ba22a4314824ba39
Daniel Cregg <danielcregg@users.noreply.github.com>
2023-11-20 00:03:18 +0000

    Refactor main method to print "Hello World!"

--- src/ie/atu/mypackage/Main.java
+++ src/ie/atu/mypackage/Main.java
@@ -1,18 +1,10 @@
-/******************************************************************************
- *  Compilation:  javac Main.java
- *  Execution:    java Main
-*
- *  Prints "Hello World!" to console.
-*
- ******************************************************************************/
 package ie.atu.mypackage;
 
-public class Main {  
-    // This is the main method.
+public class Main {
     public static void main(String[] args) {
-        
-        // This statement prints "Hello World" to the console.
+
+        // Print "Hello World!" to the console.
         System.out.println("Hello World!");
-    
+
     } // End main method
 } // End Main class
